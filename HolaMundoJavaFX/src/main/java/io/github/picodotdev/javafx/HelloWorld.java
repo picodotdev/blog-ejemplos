a01c46c8d17894c367d8eb0cadf3e69af045426a
pico.dev <pico.dev@gmail.com>
2015-10-02 18:30:01 +0200

    Pequeños retoques

--- HolaMundoJavaFX/src/main/java/io/github/picodotdev/javafx/HelloWorld.java
+++ HolaMundoJavaFX/src/main/java/io/github/picodotdev/javafx/HelloWorld.java
@@ -17,18 +17,18 @@ public class HelloWorld extends Application {
     @Override
     public void start(Stage stage) {        
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("¡Hola mundo!");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("¡Hola mundo!");
             }
         });
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         
-        stage.setTitle("Hello World!");
+        stage.setTitle("¡Hola mundo!");
         stage.setScene(new Scene(root, 300, 250));
         stage.show();
     }
