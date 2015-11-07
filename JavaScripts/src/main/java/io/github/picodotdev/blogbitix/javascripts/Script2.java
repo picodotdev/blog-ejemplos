38e8963df9bf6b7e35f7add15c315f12bfcb90ff
pico.dev <pico.dev@gmail.com>
2015-11-07 12:05:54 +0100

    Añadido ejemplo JavaScripts

---
+++ JavaScripts/src/main/java/io/github/picodotdev/blogbitix/javascripts/Script2.java
@@ -0,0 +1,8 @@
+package io.github.picodotdev.blogbitix.javascripts;
+
+public class Script2 {
+
+    public static void main(String[] args) {
+        System.out.println("Script2");
+    }
+}
---
+++ JavaScripts/src/main/java/io/github/picodotdev/blogbitix/javascripts/Script3.java
@@ -0,0 +1,8 @@
+package io.github.picodotdev.blogbitix.javascripts;
+
+public class Script3 {
+
+    public static void main(String[] args) {
+        System.out.println("Script3");
+    }
+}
