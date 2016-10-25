17969e47be60d7c1009ad529b470dcfb19d51e16
pico.dev <pico.dev@gmail.com>
2016-10-25 21:41:44 +0200

    PlugInTapestry Opciones selector de multiselect marcadas cuando todas sus opciones estén seleccionadas

--- PlugInTapestry/src/main/resources/META-INF/modules/app/multiselect.js
+++ PlugInTapestry/src/main/resources/META-INF/modules/app/multiselect.js
@@ -2,17 +2,17 @@ define("app/multiselect", ["jquery"], function($) {
 
     function Multiselect(spec) {
         this.select = $('#' + spec.clientId);
+
+        var tokensSelectors = this.select.data('tokens-selectors');
+        if (tokensSelectors == null || tokensSelectors.length == 0) {
+            return;
+        }
+        this.tokensSelectors = tokensSelectors.split(',');
         var that = this;
 
-        this.select.on('loaded.bs.select', function (event) {
-            var tokensSelectors = that.select.data('tokens-selectors');
-            if (tokensSelectors == null || tokensSelectors.length == 0) {
-                return;
-            }
-            tokensSelectors = tokensSelectors.split(',');
-
+        this.select.on('loaded.bs.select', function(event) {
             var buttons = '';
-            $.each(tokensSelectors, function(i, it) {
+            $.each(that.tokensSelectors, function(i, it) {
                 buttons += (i % 2 == 0) ? '<div class="btn-group btn-group-sm btn-block">' : '';
                 buttons += '<button type="button" data-select-custom-token="' + it + '" class="actions-btn-custom bs-select-custom btn btn-default">' + it.substr(0, 1).toUpperCase() + it.substr(1) + '</button>'
                 buttons += (i % 2 == 1 || i + 1 == tokensSelectors.length) ? '</div>' : '';
@@ -27,7 +27,22 @@ define("app/multiselect", ["jquery"], function($) {
                 var values = that.select.find('[data-tokens~=' + token + ']').filter(':not([disabled])').map(function() {
                     return $(this).val();
                 }).get();
+                values = values.concat(that.select.val());
                 that.select.selectpicker('val', values);
+                that.select.trigger('change');
+            });
+        });
+
+        this.select.on('change', function(event, clickedIndex, newValue, oldValue) {
+            $.each(that.tokensSelectors, function(i, it) {
+                var values = that.select.find('[data-tokens~=' + it + ']').filter(':not([disabled])').map(function() {
+                    return $(this).val();
+                }).get();
+                var active = values.every(function(value) {
+                    return that.select.val() !== null && that.select.val().includes(value);
+                });
+                $('button[data-id="' + spec.clientId + '"] + div[role="combobox"] div.bs-actionsbox')
+                    .find('[data-select-custom-token="' + it + '"]').toggleClass('active', active);
             });
         });
     }
