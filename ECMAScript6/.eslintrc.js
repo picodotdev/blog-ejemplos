d2d5d38ddbe1a8aff5640c21a26a482bb01b8363
pico.dev <pico.dev@gmail.com>
2016-12-09 19:28:32 +0100

    Pequeños retoques

---
+++ ECMAScript6/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+	"rules": {
+		"semi": ["error", "always"],
+		"indent": ["error", 4]
+	}
+};
