efbd692df2ba79ff8d36bc3ab4f76e35cb6fd14b
pico.dev <pico.dev@gmail.com>
2017-03-18 14:40:30 +0100

    Pequeños retoques

--- JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/Lcd.java
+++ JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/Lcd.java
@@ -4,6 +4,9 @@ import com.diozero.I2CLcd;
 import com.diozero.api.I2CConstants;
 
 import java.nio.ByteOrder;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Lcd {
 
@@ -16,8 +19,22 @@ public class Lcd {
             lcd.setCharacter(13, 0, (char) 0);
             lcd.setCharacter(14, 0, (char) 1);
             lcd.setCharacter(15, 0, (char) 2);
-            lcd.setText(1, " powered by Java");
-            Thread.sleep(20000);
+
+            Thread messager = new Thread(() -> {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        lcd.setText(1, " powered by Java");
+                        Thread.sleep(3000);
+                        lcd.setText(1, LocalDateTime.now(ZoneId.of("Europe/Madrid")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+                        Thread.sleep(3000);
+                    }
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            });
+
+            messager.start();
+            messager.join();
         }
     }
 }
