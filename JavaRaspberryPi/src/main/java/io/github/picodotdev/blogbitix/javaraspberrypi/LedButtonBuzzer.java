8e0c5b40cbd34777ff9fd27d35d7cf46f4001484
pico.dev <pico.dev@gmail.com>
2017-04-07 23:53:13 +0200

    Pequeños retoques

--- JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/LedButtonBuzzer.java
+++ JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/LedButtonBuzzer.java
@@ -5,15 +5,13 @@ import com.diozero.Buzzer;
 import com.diozero.LED;
 import com.diozero.api.GpioPullUpDown;
 
-public class ButtonLedBuzzer {
+public class LedButtonBuzzer {
 
     public static void main(String[] args) throws Exception {
-        try (LED led = new LED(21); Buzzer buzzer = new Buzzer(23, true); Button button = new Button(22, GpioPullUpDown.PULL_UP)) {
+        try (LED led = new LED(18); Buzzer buzzer = new Buzzer(21, true); Button button = new Button(22, GpioPullUpDown.PULL_UP)) {
             led.on();
-            buzzer.on();
             Thread.sleep(3000);
             led.off();
-            buzzer.off();
 
             button.whenPressed(() -> {
                 led.on();
