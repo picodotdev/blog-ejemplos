185fbff5e839cac5db5be64ba932e3909e2804aa
pico.dev <pico.dev@gmail.com>
2017-04-16 13:40:08 +0200

    Pequeños reetoques

--- DockerSwarm/06-create-volumes.sh
+++ DockerSwarm/06-create-volumes.sh
@@ -3,7 +3,7 @@
 export MACHINE_STORAGE_PATH="/run/media/picodotdev/BMOVE ROJO/docker-machine/"
 eval $(docker-machine env node-01)
 
-docker volume create --driver=rexray --name=postgresql --opt=size=5
+docker volume create --driver=rexray --name=postgres --opt=size=5
 docker volume create --driver=rexray --name=app --opt=size=5
 
 echo -e "\n# Volumes"
