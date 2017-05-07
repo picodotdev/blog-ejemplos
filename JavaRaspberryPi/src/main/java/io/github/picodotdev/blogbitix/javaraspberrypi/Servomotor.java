5b163e66d3c14f760c17f81900e153d2c8f39275
pico.dev <pico.dev@gmail.com>
2017-05-07 09:58:08 +0200

    Pequeños retoques

--- JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/Servomotor.java
+++ JavaRaspberryPi/src/main/java/io/github/picodotdev/blogbitix/javaraspberrypi/Servomotor.java
@@ -3,7 +3,7 @@ package io.github.picodotdev.blogbitix.javaraspberrypi;
 import com.diozero.LED;
 import com.diozero.sandpit.Servo;
 
-public class Servo1 {
+public class Servomotor {
 
     public static void main(String[] args) throws InterruptedException {
         try (LED led = new LED(21); Servo servo = new Servo(18, 50, 1.45F)) {
@@ -15,9 +15,9 @@ public class Servo1 {
                 Thread.sleep(3000);
                 servo.setPulseWidthMs(2.3F);
             }
+
             servo.setPulseWidthMs(1.45F);
             Thread.sleep(2000);
         }
-        Thread.sleep(2000);
     }
 }
