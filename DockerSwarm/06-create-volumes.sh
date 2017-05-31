70833bd297bfa76f78d48ddc0fecc0f1528d19ae
pico.dev <pico.dev@gmail.com>
2017-05-31 23:35:50 +0200

    Comentar una variable de entorno

--- DockerSwarm/06-create-volumes.sh
+++ DockerSwarm/06-create-volumes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MACHINE_STORAGE_PATH="/run/media/picodotdev/BMOVE ROJO/docker-machine/"
+#export MACHINE_STORAGE_PATH="/run/media/picodotdev/BMOVE ROJO/docker-machine/"
 eval $(docker-machine env node-01)
 
 docker volume create --driver=rexray --name=postgres --opt=size=5
