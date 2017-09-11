a16098ea3af45cf59e7009b470c9b5f25afbcd0c
picodotdev <pico.dev@gmail.com>
2017-09-11 13:05:03 +0200

    Añadido ejemplo Javadoc

---
+++ Javadoc/src/main/java/io/github/picodotdev/blogbitix/javadoc/Main.java
@@ -0,0 +1,21 @@
+package io.github.picodotdev.blogbitix.javadoc;
+
+/**
+ * Clase main de la aplicación.
+ *
+ * @see <a href="http://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html">Javadoc</a>
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/">Javadoc JDK</a>
+ * @author picodotdev
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Método que imprime un mensaje.
+     * 
+     * @param args Argumentos del programa
+     */
+    public static void main(String[] args) {
+      System.out.println("Hola mundo");
+    }
+}
