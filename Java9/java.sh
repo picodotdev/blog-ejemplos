17e67c6c607f15fa29909f2dacdae3d53e3eef0b
picodotdev <pico.dev@gmail.com>
2017-09-27 23:40:56 +0200

    Pequeños retoques

--- Java9/java.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-javac -d build src/helloworld/module-info.java src/helloworld/io/github/picodotdev/blogbitix/java9/helloworld/Main.java
-java --module-path build --module helloworld/io.github.picodotdev.blogbitix.java9.helloworld.Main
