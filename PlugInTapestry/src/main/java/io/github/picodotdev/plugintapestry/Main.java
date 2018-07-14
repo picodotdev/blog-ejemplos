1ebb02b9d921646e17b61f96cdc3ce9405487ac0
picodotdev <pico.dev@gmail.com>
2018-07-15 01:50:53 +0200

    PlugInTapestry: configuración de log4j2

--- PlugInTapestry/src/main/java/io/github/picodotdev/plugintapestry/Main.java
+++ PlugInTapestry/src/main/java/io/github/picodotdev/plugintapestry/Main.java
@@ -1,6 +1,8 @@
 package io.github.picodotdev.plugintapestry;
 
 import io.github.picodotdev.plugintapestry.spring.AppConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +13,18 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer implements CommandLineRunner {
 
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
 	@Override
 	public void run(String... args) {
+		StringBuilder banner = new StringBuilder();
+		banner.append("   ___  __          ____   ______                  __          \n");
+		banner.append("  / _ \\/ /_ _____ _/  _/__/_  __/__ ____  ___ ___ / /_______ __\n");
+		banner.append(" / ___/ / // / _ `// // _ \\/ / / _ `/ _ \\/ -_|_-</ __/ __/ // /\n");
+		banner.append("/_/  /_/\\_,_/\\_, /___/_//_/_/  \\_,_/ .__/\\__/___/\\__/_/  \\_, / \n");
+		banner.append("            /___/                 /_/                   /___/");
+		logger.info("\n" + banner.toString());
+		logger.info("Application runnig");
  	}
 	
     @Override
