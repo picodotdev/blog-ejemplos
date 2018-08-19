db278202605741bb47bfc6088770dc8bd7382fbf
picodotdev <pico.dev@gmail.com>
2018-08-19 11:50:55 +0200

    GraphQL: El editor, explorador e IDE GraphiQL para una API con GraphQL

--- GraphQL/src/main/java/io/github/picodotdev/blogbitix/graphql/Main.java
+++ GraphQL/src/main/java/io/github/picodotdev/blogbitix/graphql/Main.java
@@ -85,7 +85,7 @@ public class Main {
             }
         };
 
-        return new ServletRegistrationBean(new SimpleGraphQLServlet(schema, new DefaultExecutionStrategyProvider(new BatchedExecutionStrategy()), null, null, null, errorHandler, contextBuilder, null, null), "/library");
+        return new ServletRegistrationBean(SimpleGraphQLServlet.builder(schema).withExecutionStrategyProvider(new DefaultExecutionStrategyProvider(new BatchedExecutionStrategy())).withGraphQLErrorHandler(errorHandler).withGraphQLContextBuilder(contextBuilder).build(), "/library");
     }
 
     public static void main(String[] args) {
