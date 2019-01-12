408919bc96868d2ed4dd3c0576d4c40ccfc49f3a
pico.dev <pico.dev@gmail.com>
2019-01-12 16:20:43 +0100

    Jist: fixes

--- Jist/src/main/java/io/github/picodotdev/jist/Main.java
+++ Jist/src/main/java/io/github/picodotdev/jist/Main.java
@@ -12,33 +12,36 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
 
+    private String user;
     private HttpClient client;
 
-    private Main() {
-        this.client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).version(HttpClient.Version.HTTP_2).build();
+    private Main(String user) {
+        this.user = user;
     }
 
-    private void download(String user) throws Exception {
-        List<JsonObject> gists = getUserGists(user);
+    private void download() throws Exception {
+        List<JsonObject> gists = getUserGists();
         downloadGists(gists);
     }
 
-    private List<JsonObject> getUserGists(String user) throws Exception {
+    private List<JsonObject> getUserGists() throws Exception {
         List<JsonObject> gists = new ArrayList<>();
         int page = 1;
         boolean more = true;
 
         do {
-            List<JsonObject> array = getUserGists(user, page);
+            List<JsonObject> array = getUserGists(page);
             gists.addAll(array);
             more = array.size() > 0;
             page += 1;
@@ -47,9 +50,9 @@ public class Main {
         return gists;
     }
 
-    private List<JsonObject> getUserGists(String user, int page) throws Exception {
-        String userGistsUrl = String.format("https://api.github.com/users/%s/gists?page=%s", user, page);
-        HttpResponse<String> response = client.send(HttpRequest.newBuilder(new URI(userGistsUrl)).GET().build(), HttpResponse.BodyHandlers.ofString());
+    private List<JsonObject> getUserGists(int page) throws Exception {
+        String userGistsUrl = String.format("https://api.github.com/users/%s/gists?page=%s&per_page=100", user, page);
+        HttpResponse<String> response = getClient().send(HttpRequest.newBuilder(new URI(userGistsUrl)).GET().build(), HttpResponse.BodyHandlers.ofString());
         JsonReader gistsJsonReader = Json.createReader(new StringReader(response.body()));
         JsonArray gistsArray = gistsJsonReader.readArray();
         return gistsArray.getValuesAs(JsonObject.class);
@@ -58,6 +61,7 @@ public class Main {
     private void downloadGists(List<JsonObject> gists) throws Exception {
         for (JsonObject gist : gists) {
             String id = gist.getString("id");
+            System.out.println(id);
             LocalDateTime creationDate = LocalDateTime.parse(gist.getString("created_at"), DateTimeFormatter.ISO_ZONED_DATE_TIME);
             JsonObject files = gist.getJsonObject("files");
 
@@ -73,7 +77,15 @@ public class Main {
         String year = String.valueOf(date.getYear());
         Path path = Paths.get("gists", year, id, fileName);
         path.toFile().getParentFile().mkdirs();
-        client.send(HttpRequest.newBuilder(new URI(url)).GET().build(), HttpResponse.BodyHandlers.ofFile(path));
+        getClient().send(HttpRequest.newBuilder(new URI(url)).GET().build(), HttpResponse.BodyHandlers.ofFile(path));
+    }
+
+    private String getAuthorization(String user, String password) {
+        return "Basic: " + Base64.getEncoder().encodeToString(String.format("%s:%s", user, password).getBytes());
+    }
+
+    private HttpClient getClient() {
+        return HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).version(HttpClient.Version.HTTP_2).build();
     }
 
     private static Arguments parseArgs(String[] args) {
@@ -86,10 +98,9 @@ public class Main {
 
         return arguments;
     }
-
-
+    
     public static void main(String[] args) throws Exception {
         Arguments arguments = parseArgs(args);
-        new Main().download(arguments.getUser());
+        new Main(arguments.getUser()).download();
     }    
 }
