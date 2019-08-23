2dd966dc51f5833694cddcc46572ed54fa6bbdad
pico.dev <pico.dev@gmail.com>
2019-08-23 11:53:40 +0200

    SpringCloudVault: Artículo #429

---
+++ SpringCloudVault/src/main/java/io/github/picodotdev/blogbitix/springcloudvault/Main.java
@@ -0,0 +1,44 @@
+package io.github.picodotdev.blogbitix.springcloudvault;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Override
+    public void run(String... args) {
+        System.out.printf("Username: %s%n", username);
+        System.out.printf("Passowrd: %s%n", password);
+
+        boolean test = false;
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/app", username, password)) {
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+}
