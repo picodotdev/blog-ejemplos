587d7ee500b1bc4175c5d1ba294a9cf408952f2f
pico.dev <pico.dev@gmail.com>
2020-02-28 19:31:05 +0100

    Log4j: Artículo #466

--- Log4j/src/main/java/io/github/picodotdev/blogbitix/log4j/Main.java
+++ Log4j/src/main/java/io/github/picodotdev/blogbitix/log4j/Main.java
@@ -1,7 +1,9 @@
 package io.github.picodotdev.blogbitix.log4j;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.util.Arrays;
 
@@ -10,6 +12,11 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        Main.logReplace();
+        Main.changeLogLevelDynamically();
+    }
+
+    private static void logReplace() {
         Product product = new Product(1l, "Intel NUC", "Negro");
 
         logger.info("Product({}, {})", product.getId(), product.getName());
@@ -18,5 +25,15 @@ public class Main {
         logger.info(new ProductMessage(product));
         logger.info(new SecuredMessage("Tarjeta de crédito: 1111 1111 1111 1111, DNI: 11111111A", Arrays.asList("(\\d{4} \\d{4} \\d{4} \\d{1})(?=\\d{3})", "(\\d{6})(?=\\d{2}[A-Z])")));
         logger.info("Tarjeta de crédito: 1111 1111 1111 1111, DNI: 11111111A");
+    }
+
+    private static void changeLogLevelDynamically() {
+        Configurator.setLevel(logger.getName(), Level.ERROR);
+        logger.info("Level.ERROR info trace");
+        logger.error("Level.ERROR error trace");
+
+        Configurator.setLevel(logger.getName(), Level.INFO);
+        logger.info("Level.INFO info trace");
+        logger.error("Level.INFO error trace");
     }
 }
