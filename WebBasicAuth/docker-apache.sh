c1bbd54fb8bfa28fc76e4f94f3aaa5c54a41fd8a
pico.dev <pico.dev@gmail.com>
2020-08-02 13:32:35 +0200

    Ejemplo WebBasicAuth: Artículo #505

---
+++ WebBasicAuth/docker-apache.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+docker run --rm -p 80:80 -p 443:443 \
+  -v `pwd`/httpd.conf:/usr/local/apache2/conf/httpd.conf:ro \
+  -v `pwd`/httpd-vhosts.conf:/usr/local/apache2/conf/extra/httpd-vhosts.conf:ro \
+  -v `pwd`/localhost.crt:/usr/local/apache2/conf/extra/localhost.crt:ro \
+  -v `pwd`/localhost.key:/usr/local/apache2/conf/extra/localhost.key:ro \
+  -v `pwd`/.localhost.htpasswd:/usr/local/apache2/conf/extra/.localhost.htpasswd:ro \
+  httpd:alpine
+
+# docker cp 7697c6d3eeff:/usr/local/apache2/conf/httpd.conf httpd.conf
---
+++ WebBasicAuth/docker-nginx.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker run --rm -p 80:80 -p 443:443 \
+  -v `pwd`/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+  -v `pwd`/localhost.crt:/etc/nginx/conf.d/localhost.crt:ro \
+  -v `pwd`/localhost.key:/etc/nginx/conf.d/localhost.key:ro \
+  -v `pwd`/.localhost.htpasswd:/etc/nginx/conf.d/.localhost.htpasswd:ro \
+  nginx:alpine
