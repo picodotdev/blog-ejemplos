1a2267b4aa64eceb429fabb18ecbecd5351005f8
pico.dev <pico.dev@gmail.com>
2020-10-16 21:29:01 +0200

    EventBus: Pequeños retoques

--- EventBus/src/main/java/io/github/picodotdev/blogbitix/eventbus/domain/shared/querybus/QueryHandler.java
+++ EventBus/src/main/java/io/github/picodotdev/blogbitix/eventbus/domain/shared/querybus/QueryHandler.java
@@ -1,6 +1,6 @@
 package io.github.picodotdev.blogbitix.eventbus.domain.shared.querybus;
 
-public interface QueryHandler<T,U extends Query> {
+public interface QueryHandler<T,U extends Query<T>> {
 
     T handle(U query) throws Exception;
 }
