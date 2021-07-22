d710f91d271193b610f99dbae0c67d64a5881466
pico.dev <pico.dev@gmail.com>
2021-07-22 19:43:03 +0200

    JavaRandom: Pequeños retoques

--- JavaRandom/app/src/main/java/io/github/picodotdev/blogbitix/javarandom/Main.java
+++ JavaRandom/app/src/main/java/io/github/picodotdev/blogbitix/javarandom/Main.java
@@ -7,19 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Random");
-        String randomNumbers = IntStream.rangeClosed(1, 10).mapToObj(i -> Integer.toString(RandomUtil.getInt(0, 10))).collect(Collectors.joining(", "));
+        String randomNumbers = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Integer.toString(RandomUtil.getInt(0, 10)))
+                .collect(Collectors.joining(", "));
         System.out.printf("Numbers: %s%n", randomNumbers);
 
         System.out.println("\nStream");
-        String streamNumbers = RandomUtil.getIntStream(1, 10, 10).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
+        String streamNumbers = RandomUtil.getIntStream(1, 10, 10)
+                .mapToObj(i -> Integer.toString(i))
+                .collect(Collectors.joining(", "));
         System.out.printf("Numbers: %s%n", streamNumbers);
 
         System.out.println("\nMath");
-        String mathNumbers = IntStream.rangeClosed(1, 10).mapToObj(i -> Integer.toString(RandomUtil.getIntMath(0, 10))).collect(Collectors.joining(", "));
+        String mathNumbers = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Integer.toString(RandomUtil.getIntMath(0, 10)))
+                .collect(Collectors.joining(", "));
         System.out.printf("Numbers: %s%n", mathNumbers);
 
         System.out.println("\nUUID");
-        String uuidNumbers = IntStream.rangeClosed(1, 10).mapToObj(i -> RandomUtil.getUUID().toString()).collect(Collectors.joining(", "));
+        String uuidNumbers = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> RandomUtil.getUUID().toString())
+                .collect(Collectors.joining(", "));
         System.out.printf("Numbers: %s%n", uuidNumbers);
     }
 }
