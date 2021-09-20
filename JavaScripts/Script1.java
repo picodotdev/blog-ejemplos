8f310f34b5133fa69c66526533b215ddd6d4e02e
pico.dev <pico.dev@gmail.com>
2021-09-20 22:08:46 +0200

    JavaScripts: Incluir ejemplo con JBang

---
+++ JavaScripts/Script1.java
@@ -0,0 +1,14 @@
+///usr/bin/env jbang --quiet "$0" "$@" ; exit $? 
+
+//DEPS org.apache.commons:commons-lang3:3.4
+
+package io.github.picodotdev.blogbitix.javascripts;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Script1 {
+
+    public static void main(String[] args) {
+        System.out.printf("Script1 %s%n", StringUtils.join(args));
+    }
+}
